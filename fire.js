56314081dd8294e1086ae5f5034d6ae233f78f10
stivens73 <stivens54@gmail.com>
2018-11-03 18:57:41 -0700

    setup firebase config

--- fire.js
+++ fire.js
@@ -2,13 +2,14 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
-};
+    apiKey: "AIzaSyC3m1LnKXudteB2WKCToWZ-0-_cVgp-NNU",
+    authDomain: "edva-cmpe172.firebaseapp.com",
+    databaseURL: "https://edva-cmpe172.firebaseio.com",
+    projectId: "edva-cmpe172",
+    storageBucket: "edva-cmpe172.appspot.com",
+    messagingSenderId: "606646992739"
+}
+
 const fire= firebase.initializeApp(config);
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
