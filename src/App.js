10a560507de4f7ba94e9ed9ad8b55f892294bf10
stivens73 <stivens54@gmail.com>
2018-11-04 15:01:14 -0800

    setting up SSO

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './App.css';
+// import LandingPage from './stateful_components/landingPage/landingpage'
+class App extends Component {
+    render() {
+        return (
+            <div className='app'>
+                <LandingPage />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/login/login.js
@@ -0,0 +1,71 @@
+import React,{Component} from 'react'
+import {Header,Form,Segment,Button} from 'semantic-ui-react';
+import './loginModal.css';
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+
+class LoginModal extends Component {
+    constructor(props){
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.state={
+            email:'',
+            password:'',
+        };
+
+    }
+    componentDidMount(){
+
+    }
+
+    uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+            signInSuccess: function() {
+                console.log(this.state.email);
+                this.props.userLogin(this.state.email);
+            }.bind(this)
+        }
+    }
+//this.props.userLogin(this.state.email);
+
+    onChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value});
+    };
+
+    render() {
+
+        return(
+            <div id='form'>
+        <Header as='h2' style={{color: "white"}} textAlign='center'>
+            {' '}Sign-in to your account
+        </Header>
+        <Form size='large'>
+            <Segment raised>
+        <StyledFirebaseAuth
+        uiConfig={this.uiConfig}
+        firebaseAuth={firebase.auth()}
+        />
+        <Button
+        name='guestLogin'
+        onClick={this.props.guestLogin}
+        color='green'
+
+        size='large'>Continue as guest</Button>
+            <br></br>
+
+            </Segment>
+
+            </Form>
+            </div>
+
+
+    )}
+}
+export default LoginModal;
