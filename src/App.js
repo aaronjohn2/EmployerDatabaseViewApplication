39c65c2ed19f0e8249ca1625a217cf228aa65268
stivens73 <stivens54@gmail.com>
2018-11-04 16:43:29 -0800

    jenkins test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import './App.css';
-// import LandingPage from './stateful_components/landingPage/landingpage'
+import './App.css';
+import LandingPage from './stateful_components/landingPage/landingpage'
 class App extends Component {
     render() {
         return (
