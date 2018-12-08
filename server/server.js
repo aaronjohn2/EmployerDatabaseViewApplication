9d590b07eb1c945007d21edade58a6158a990e37
wmatl <wmatlob@yahoo.com>
2018-12-08 15:53:48 -0800

    Connecting to the professor mysql sample data

--- server/server.js
+++ server/server.js
@@ -40,64 +40,16 @@ db.once("open", () => console.log("connected to the database"));
 // checks if connection with the database is successful
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// (optional) only made for logging and
+// Connecting to mysql
+
+const mysql = require('mysql2');
+
 // bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(cors());
-/*
 
-// this is our get method
-// this method fetches all available data in our database
-router.get("/getData", (req, res) => {
-    Data.find((err, data) => {
-        if (err) return res.json({ success: false, error: err });
-        return res.json({ success: true, data: data });
-    });
-});
-
-// this is our update method
-// this method overwrites existing data in our database
-router.post("/updateData", (req, res) => {
-    const { id, update } = req.body;
-    Data.findOneAndUpdate(id, update, err => {
-        if (err) return res.json({ success: false, error: err });
-        return res.json({ success: true });
-    });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete("/deleteData", (req, res) => {
-    const { id } = req.body;
-    Data.findOneAndDelete(id, err => {
-        if (err) return res.send(err);
-        return res.json({ success: true });
-    });
-});
-
-// this is our create methid
-// this method adds new data in our database
-router.post("/putData", (req, res) => {
-    let data = new Data();
-
-    const { id, message } = req.body;
-
-    if ((!id && id !== 0) || !message) {
-        return res.json({
-            success: false,
-            error: "INVALID INPUTS"
-        });
-    }
-    data.message = message;
-    data.id = id;
-    data.save(err => {
-        if (err) return res.json({ success: false, error: err });
-        return res.json({ success: true });
-    });
-});
-*/
 
 // append /api for our http requests
 app.use("/api", router);
@@ -121,6 +73,33 @@ app.use(express.static('public'));
 // app.get('/data', (req, res) =>
 //     res.send(`Node and express server are running on port ${API_PORT}`)
 // );
+// sql connection
 
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'dragon',
+    database: 'employees'
+});
+try{
+connection.connect((err) =>{
+        if (err) throw err;
+        console.time('mysql');
+        console.log('Successfully connected to mysql');
+    try{
+        connection.query('SELECT * FROM employees LIMIT 100', (err, results, fields) =>{
+        if (err) throw err;
+        console.log(fields);
+        console.log(results);
+        })} catch (e) {
+            console.log(err)
+        }
+        connection.end();
+    })
+} catch (e) {
+    console.log(err)
+    }
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+
